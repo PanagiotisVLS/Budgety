d621a5748f34c3ce8d1400280da64c0bb24feae1
panagiotis <ptrianta@olympusmarinegroup.com>
2019-08-16 18:54:49 +0300

    More Code - Complted initialization

--- app.js
+++ app.js
@@ -49,30 +49,43 @@ Global App Controller
 
 var controller = (function(budgetCtrl, UICtrl) 
 {
-    var DOM = UICtrl.getDOMstrings();
+    // Initializes the events listeners
+    var setupEventListeners = function()
+    {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
     
+        document.addEventListener("keypress", function(event) 
+        {
+            if (event.keyCode === 13 || event.which === 13)
+            {
+                ctrlAddItem();
+            }    
+        });
+    }
+    
+    // Gets called when we want to add a new item
     var ctrlAddItem = function()
     {
         // 1. Get the field input data
         var input = UICtrl.geinput();
-        console.log(input);
+        
         // 2. Add the item to the button controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
         console.log("It works");
-    }
+    }        
     
-    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-    
-    document.addEventListener("keypress", function(event) 
-    {
-        if (event.keyCode === 13 || event.which === 13)
+    return {
+        init: function()
         {
-            ctrlAddItem();
+            console.log("Application has started.");
+            setupEventListeners();
         }
-        
-            
-    });
-                                                         
+    }
 })(budgetController, UIController);
+
+// Initializes the controller which includes the code
+controller.init();
