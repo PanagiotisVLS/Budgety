d32f57ecefbfd7fbcb0faac45594c2072e5dc391
panagiotis <ptrianta@olympusmarinegroup.com>
2019-08-16 19:10:42 +0300

    Created Constructors

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ Budget Controller
 
 var budgetController = (function()
 {
+    // Function Constructor for the records
+    var Expense = function(id, description, value)
+    {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income = function(id, description, value)
+    {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    
+    
+    var data = {
+        allItems: 
+        {
+            exp: [],
+            inc: []
+        },
+        
+        totals: 
+        {
+            exp: 0,
+            inc: 0
+        }
+    }
+    
 })();
 
 
