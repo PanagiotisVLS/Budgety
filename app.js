087085cb9300d9da0dc62d5355c46e412177c873
panagiotis <ptrianta@olympusmarinegroup.com>
2019-08-16 19:57:31 +0300

    More code

--- app.js
+++ app.js
@@ -33,7 +33,46 @@ var budgetController = (function()
             exp: 0,
             inc: 0
         }
-    }
+    };
+    
+    return {
+        addItem: function(type, description, value)
+        {
+            var newItem, ID;
+            
+            // Create new ID
+            if (data.allItems[type].length > 0)
+            {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;    /***************** Q&a *******************************/
+            }
+            else
+            {
+               ID = 0; 
+            }
+            
+            
+            // Create new item based on type
+            if(type === "exp")
+            {
+                newItem = new Expense(ID, description, value);
+            }
+            else if (type === "inc")
+            {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            // Return the new element
+            return newItem;
+        },
+        
+        testing: function()
+        {
+            console.log(data);
+        }
+    };
     
 })();
 
@@ -98,10 +137,14 @@ var controller = (function(budgetCtrl, UICtrl)
     // Gets called when we want to add a new item
     var ctrlAddItem = function()
     {
+        var input, newItem;
+        
         // 1. Get the field input data
-        var input = UICtrl.geinput();
+        input = UICtrl.geinput();
         
         // 2. Add the item to the button controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
